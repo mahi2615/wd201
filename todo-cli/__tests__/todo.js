5b2f048a06b24661770161d993471f685f08c9dc
mahi2615 <151551820+mahi2615@users.noreply.github.com>
2023-12-20 18:56:00 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -10,23 +10,23 @@ const todoList = require("../todo");
 const { all, markAsComplete, add, overdue,
     dueToday,
     dueLater } = todoList();
-const today = new Date(); 
-const oneDay = 60 * 60 * 24 * 1000;
+const TOday = new Date(); 
+const ONEDay = 60 * 60 * 24 * 1000;
 describe("todoList", () => {
   beforeAll(() => {
-    const today = new Date();
+    const TOday = new Date();
      //referred to discord forum for this line of code
     add({
       title: "Test todo",
       completed: false,
-      dueDate: new Date(today.getTime() - 1 * oneDay).toLocaleDateString(
+      dueDate: new Date(TOday.getTime() - 1 * ONEDay).toLocaleDateString(
         "en-CA",
       ),
     });
     add({
       title: "Test todo2",
       completed: false,
-      dueDate: new Date(today.getTime() + 1 * oneDay).toLocaleDateString(
+      dueDate: new Date(TOday.getTime() + 1 * ONEDay).toLocaleDateString(
         "en-CA",
       ),
     });
@@ -55,7 +55,7 @@ describe("todoList", () => {
     add({
         title: "Test todo",
         completed: false,
-        dueDate: new Date(today.getTime() - 1 * oneDay).toLocaleDateString(
+        dueDate: new Date(TOday.getTime() - 1 * ONEDay).toLocaleDateString(
           "en-CA",
         ),
       });
@@ -75,7 +75,7 @@ describe("todoList", () => {
     add({
         title: "Test todo2",
         completed: false,
-        dueDate: new Date(today.getTime() + 2 * oneDay)
+        dueDate: new Date(TOday.getTime() + 2 * ONEDay)
         .toISOString()
         .slice(0, 10),
       });
